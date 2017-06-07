8b8c9b5c032d304f96caaa59c22dfdf00b6ba013
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2017-06-07 17:46:46 +0900

    Add command to print version number

---
+++ cmd/version.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func versionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("ecs-goploy v0.1.0")
+		},
+	}
+
+	return cmd
+}
